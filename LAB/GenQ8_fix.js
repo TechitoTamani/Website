c6447d4f749d2d482e16b882fd9d767d2c7ecbbc
Tech-it-o <techitotamani.irl@gmail.com>
2025-03-15 21:16:57 +0700

    add Lab

---
+++ LAB/GenQ8_fix.js
@@ -0,0 +1,82 @@
+import { AnsNotIn,getRandomInt,getRandomIntMN,ArrayEqual,ArrayNotIn } from "/QuiZGenQ/QData/GobalFunction.js";
+
+export function GenRandomQ8(){
+
+    let Ans = [];
+    let TF = [false,false,false,false]
+
+    const namelist = ["ชิ","วิน","สายฟ้า","โฟ","ต้นตาล","ภีม"]
+    const question_text = "ในการปรับให้หยดน้ำ หยดจากปลายหลอดบิวเรตต์ชนิดหนึ่ง เมื่อหยดถึงพื้นอีกหยดหนึ่งถัดไปก็หยดทันที เมื่อปลายบิวเรตต์สูง h ซม. หยดน้ำจะหยดได้กี่หยดต่อ t วินาที".split(" ")
+
+    // QuestionCode
+
+    let Name = namelist[getRandomInt((namelist.length)-1)]
+
+    let t = (getRandomIntMN(1, 20))
+    let h = (getRandomIntMN(1, 100))
+
+    while (!Number.isInteger(10*t*((5/h)**(1/2)))) {
+        t = (getRandomIntMN(1, 20));
+        h = (getRandomIntMN(1, 100));
+    }
+
+
+    for (let i = 0; i < question_text.length; i++) {
+        if (question_text[i] == "name"){
+            question_text[i] = Name
+        }
+        else if (question_text[i] == "t"){
+            question_text[i] = t
+        }
+        else if (question_text[i] == "h"){
+            question_text[i] = h
+        }
+    }
+
+    // Anscode
+
+    let result = 10*t*((5/h)**(1/2))
+    let choice = [result]
+
+    // RandomAnsCode
+
+    let choiceAmount = 4
+
+    while (choice.length < choiceAmount) {
+        let WrongChoice = choice[(getRandomIntMN(0,(choice.length)-1))]+(1*((-1)**(getRandomIntMN(0,1))))
+        
+        while (WrongChoice <= 0) {
+            WrongChoice = choice[(getRandomIntMN(0,(choice.length)-1))]+(1*((-1)**(getRandomIntMN(0,1))))
+        }
+
+        if (AnsNotIn(choice,WrongChoice)) {
+            choice = [...choice,WrongChoice]
+        }
+    }
+
+    // PrintCode
+
+    let questionPrint = question_text.join(" ")
+
+    choice.sort()
+    
+    for (let j = 0; j < choice.length; j++) {
+
+        let textAns = ([j+1,choice[j]].join(") "))
+
+        if (choice[j] == result){
+            Ans = [...Ans,{ text: textAns, correct: true}]
+        }
+        else {
+            Ans = [...Ans,{ text: textAns, correct: false}]
+        }
+    }
+
+    let box = {
+        question: questionPrint,
+        answers: Ans
+    };
+    // console.log(box) // check
+
+    return box ;
+}
